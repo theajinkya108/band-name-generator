3e50cab036128dc8599577f69424d2f4d21dcb9b
Ajinkya Bhoite <ajinkyabh2203@gmail.com>
2025-01-25 21:07:09 +0530

    fix(existing admin app removed) : from INSTALLED_APPS

--- band_name_generator/settings.py
+++ band_name_generator/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = ['.onrender.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
