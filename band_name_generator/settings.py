9f8c998f8a84de8fccb33f5acc0258386f5f8979
Ajinkya Bhoite <ajinkyabh2203@gmail.com>
2025-01-31 13:47:33 +0530

    fix(added localhost in ALLOWED_HOSTS)

--- band_name_generator/settings.py
+++ band_name_generator/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-b&0-cp^oqm&+cxj3_tyn&0(+$a9@hka1z^2juvsm7r9kgzhcw5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
